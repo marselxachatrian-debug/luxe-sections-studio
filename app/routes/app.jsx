15e1691f6b5ff04b7fb8d9f0d5f285c2fb64bd93
LSS <marselxachatrian@gmail.com>
2026-03-28 08:05:28 +0300

    Use Link in embedded app navigation

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,5 +1,5 @@
 import { NavMenu } from "@shopify/app-bridge-react";
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 import { authenticate } from "../shopify.server";
 
 export async function loader({ request }) {
@@ -11,12 +11,12 @@ export default function AppLayout() {
   return (
     <>
       <NavMenu>
-        <a href="/app" rel="home">
+        <Link to="/app" rel="home">
           Dashboard
-        </a>
-        <a href="/app/blocks">Blocks Library</a>
-        <a href="/app/pricing">Pricing</a>
-        <a href="/app/settings">Settings</a>
+        </Link>
+        <Link to="/app/blocks">Blocks Library</Link>
+        <Link to="/app/pricing">Pricing</Link>
+        <Link to="/app/settings">Settings</Link>
       </NavMenu>
 
       <Outlet />
